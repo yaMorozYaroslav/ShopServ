5302b067b05500556c92763bbc26aac9fcfca1db
yaMorozYaroslav <80963959+yaMorozYaroslav@users.noreply.github.com>
2022-02-02 17:56:22 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://Yaroslav:Hor3ath3@cluster0.tlfsw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://user:password@cluster0.tlfsw.mongodb.net/yourDB?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
