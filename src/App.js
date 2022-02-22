aea7bbca0f5f581b654ea44d8c830058694a99c3
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-02-22 17:41:36 +0100

    44

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React from 'react';
+export const App = () => {
+   let data2 = [];
+   let products = [
+   {
+    condition: good
+   }]
+   
+   for(let i=0; i<arrays.length; i++){
+    data2.push(arrays(i))
+   };
+   
+    return <h1>{data2}</h1>;
+ };
---
+++ src/styles/AppStyle.js
@@ -0,0 +1,4 @@
+import styled from 'styled-components';
+export const HeaderWrap = styled.section`
+text-align: center;
+`
