3837d18f05613c301d5395fe9fae5f8eddc91838
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-02-27 15:24:33 +0100

    78

--- src/components/Listener.js
+++ src/components/Listener.js
@@ -1,6 +1,30 @@
-import React from 'react';
-export function App(){
+import React, {useState, useEffect, useRef} from 'react';
+export function Listener(){
     const [hoverRef, isHovered] = useHover();
 
-    return<div ref={hoverRef}>{isHovered?"12":"13"}</div>;
+    return<div ref={hoverRef}>{isHovered?"Go out"
+                                        :"Touch"}</div>;
+}
+function useHover(){
+    const [value, setValue] = useState(false);
+    const ref = useRef(null);
+    const handleMouseOver=()=>setValue(true);
+    const handleMouseOut=()=>setValue(false);
+
+useEffect(
+   ()=>{
+    const something = ref.current;
+    if(something){
+        something.addEventListener("mouseover", handleMouseOver);
+        something.addEventListener("mouseout", handleMouseOut);
+
+    return()=>{
+        something.removeEventListener("mouseover", handleMouseOver);
+        something.removeEventListener("mouseout", handleMouseOut);
+     };
+    }
+   },
+  [ref.current]
+   );
+ return[ref, value];
 }
