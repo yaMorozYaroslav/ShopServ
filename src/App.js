c46f76117efaf95852f862bc700fc7c5edcabe64
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-02-27 15:59:15 +0100

    98

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import MainTogl from './Gateway/MainTogl/MainTogl';
 import {Header} from './components/Header/Header';
-
+import {UseEffect} from './components/UseEffect';
+//import {Listener} from './components/Listener';
 export default class App extends React.Component{
   render(){
     return(
        <>
     <Header />
     <MainTogl />
+    <UseEffect />
        </>
       )
   }
---
+++ src/components/UseEffect.js
@@ -0,0 +1,26 @@
+import React, {useState, useEffect, useRef} from 'react';
+export function UseEffect(){
+	const [refHov, hovered] = usePast();
+	return <h1 ref={refHov}> {hovered?"Go out":"Touch"}</h1>;
+}
+function usePast(){
+	const [hover, setHov] = useState(false);
+	const path = useRef(null);
+	const handOver=()=>setHov(true);
+	const handOut=()=>setHov(false);
+
+useEffect(()=>{
+	const refObj = path.current
+	if(refObj){
+		refObj.addEventListener("mouseover", handOver);
+		refObj.addEventListener("mouseout", handOut);
+
+	  return()=>{
+	  	refObj.removeEventListener("mouseover", handOver);
+	  	refObj.removeEventListener("mouseout", handOut);
+	  };
+	}
+}, [path.current]);
+
+  return[path, hover];
+}
