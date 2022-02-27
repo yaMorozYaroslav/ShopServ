7b10db541178bf005fa2127db243b9b960d06058
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-02-27 11:20:41 +0100

    67

--- src/components/Blur.js
+++ src/components/Blur.js
@@ -38,7 +38,7 @@ export default class BlurExample extends React.Component{
        <ul>
           <li>Option 1</li>
           <li>Option 2</li>
-          <li>Oprion 3</li>
+          <li>Option 3</li>
        </ul>
 	 	)}
      </div>
