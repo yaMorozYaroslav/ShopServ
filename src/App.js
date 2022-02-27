468ae54a4d37f6f729ddb12b4a42ecb01cc2c630
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-02-27 07:43:13 +0100

    67

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import './App.css';
 import MainTogl from './Gateway/MainTogl/MainTogl';
 import {Header} from './components/Header/Header';
-//import Calculator from './Calculator/Calculator';
-//import Form from './components/Form'
+
 export default class App extends React.Component{
   render(){
     return(
---
+++ src/components/Blur.js
@@ -0,0 +1,47 @@
+import React from 'react';
+export default class BlurExample extends React.Component{
+	constructor(props){
+		super(props);
+
+		this.state={isOpen:false};
+		this.timeOutId=null;
+
+		this.onClickHandler = this.onClickHandler.bind(this);
+		this.onBlurHandler = this.onBlurHandler.bind(this);
+		this.onFocusHandler = this.onFocusHandler.bind(this);
+	}
+	onClickHandler(){
+		this.setState(currentState=>({
+			isOpen: !currentState.isOpen
+		}));
+	}
+	onBlurHandler(){
+		this.timeOutId = setTimeout(()=>{
+			this.setState({
+				isOpen:false
+			});
+		});
+	}
+	onFocusHandler(){
+		clearTimeout(this.timeOutId);
+	}
+	render(){
+		return(
+	 <div onBlur={this.onBlurHandler}
+	      onFocus={this.onFocusHandler}>
+	 <button onClick={this.onClickHandler}
+	         aria-haspopup="true"
+	         aria-expanded={this.state.isOpen}>
+	 Select an option
+	 </button>
+	 {this.state.isOpen && (
+       <ul>
+          <li>Option 1</li>
+          <li>Option 2</li>
+          <li>Oprion 3</li>
+       </ul>
+	 	)}
+     </div>
+	 );
+	}
+}
