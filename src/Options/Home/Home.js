0d14b885661037524eeaa97aa3afaffc12bedbb8
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-03-01 18:19:27 +0100

    57

--- src/Options/Home/Home.js
+++ src/Options/Home/Home.js
@@ -1,4 +1,10 @@
 import React from "react";
 export const Home = () =>{
-	return <h1>Home1</h1>
+	return <p>it's just text. just the perfect text
+	 for you. now you can read it and enjoy reading.
+nothing superfluous or additional, perfect simple 
+text for entertainment.
+I hope you are happy that you read this. I want to wish
+ you a good day, but I won't, because it's 
+ just text without any additional information.</p>
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {render} from 'react-dom';
 import {Header} from'./HeadFoot/Header/Header';
 import MainTogl from './Gateway/MainTogl/MainTogl';
+import {UseEffect} from './storage/UseEffect/UseEffect';
 
 const App=()=>{
   return(
     <>
   <Header />
   <MainTogl/>
-  
+  <UseEffect/>
     </>
     )
 }
