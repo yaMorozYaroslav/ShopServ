823035ac5524c67eb83f9f7382329bed26d0d00e
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-03-02 20:59:28 +0100

    87

--- src/HeadFoot/Header/Header.js
+++ src/HeadFoot/Header/Header.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import styles from './Header.module.css';
-export const Header = () => {
+function DuoHead(props) {
 	return(
-   <div className={styles.head}>
-      <div className={styles.block}>
-     <h1 >Development</h1>
-     <i className="fa fa-gear fa-3x"></i>
-     </div>
+   <div className={styles.double}>
+    <div>{props.one}</div>
+    <div>{props.two}</div>
    </div>
 		)
+}
+export function Header(){
+  return(
+  <DuoHead
+    one ={<div className={styles.head}>
+           <div className={styles.block}>
+           <h1 >Development</h1>
+           <i className="fa fa-gear fa-3x"></i>
+         </div>
+        </div>}
+         
+   two={<div className={styles.nohead}>Something </div>} />
+   )
 }
