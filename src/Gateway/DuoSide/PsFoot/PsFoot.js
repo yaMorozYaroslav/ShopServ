0b537849c222022d390ce139c81294218a7c35f6
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-03-06 17:26:30 +0100

    56

--- src/Gateway/DuoSide/PsFoot/PsFoot.js
+++ src/Gateway/DuoSide/PsFoot/PsFoot.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import styles from './PsFoot.module.css';
+import pic from './git.png';
 export function PsFoot(){
-	return<div className={styles.foot}>My Git</div>
-}
+	return <div className={styles.container}>
+	<img className={styles.foot} src={pic} />
+          </div>
+
+      }
