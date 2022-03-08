b351f857ce06c93761087f6af7f9205a43bd4424
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-03-08 13:44:44 +0100

    42

--- src/Options/Profile/Profile.js
+++ src/Options/Profile/Profile.js
@@ -2,11 +2,12 @@ import React from 'react';
 export const Profile = () => {
 	return <h1>I am a self-taught NodeJS developer who learns the
 	 entire stack to understand the overall
-	 development process. The 
-	 ability to use JS for both the backend and the frontend 
-	 inspires me to do this because I can dive deeper by focusing
-	  on one language. Mainly interested in creating websites 
-	  and applications. In the future, I want to take part in 
-	  the creation of web applications with artificial intelligence.
-	 Waiting for new acquaintances and profitable offers.</h1>
+	 development process. The ability to use JS for both the 
+	 backend and the frontend inspires me to do this, because I 
+	 can dive deeper while focusing on one language. Mainly 
+	 interested in creating websites and applications. In the 
+	 future, I wanna participate in the creation of AI web 
+	 applications. It is a long way, but why not if there
+	 is a lifetime. Waiting for new acquaintances and profitable
+	  offers.</h1>
 }
