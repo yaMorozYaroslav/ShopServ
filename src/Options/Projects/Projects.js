c4d5130d8ed1ad9f29e973c9aa83452670e5dfd7
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-03-08 20:56:04 +0100

    34

--- src/Options/Projects/Projects.js
+++ src/Options/Projects/Projects.js
@@ -1,8 +1,21 @@
 import React, {useState} from 'react';
+import {useEff} from './useEff';
 
 export function Projects(){
-
+ return (<>
+ <Project1/>
+ <Project2/>
+        </>)
 }
  function Project1(){
- 	const [show, setShow] = useState(false)
+ 	const [useRef, open] = useEff();
+ 	return <div ref={useRef}>{open?<h1>dick</h1>: 'dick'}</div>
+ }
+ function Project2(){
+ 	const [useRef, open] = useEff();
+ 	return <div ref={useRef}>{!open?<h1>tick</h1>: 'pick'}</div>
+ }
+  function Project3(){
+ 	const [useRef, open] =  useEff();
+ 	return <div ref={useRef}>{!open?<h1>tick</h1>: 'pick'}</div>
  }
