632bb5d0b6e6e5347e13b5e8b3a9e35085d0fd69
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-03-10 20:36:05 +0100

    14

--- src/Options/Contacts/Contacts.js
+++ src/Options/Contacts/Contacts.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import styles from './Contacts.module.css';
 export function Contacts(){
-	const Number = () =>{return (<><h1>Number</h1><p>+48606259667</p></>)};
+	const Number = () =>{
+		return (<><h1 className={styles.label}>Number</h1>
+			      <p className={styles.discr}>+48606259667</p></>)};
 
-	const Email = () =>{return (<><h1>Email</h1><p>ya.moroz.yaroslav@gmail.com</p></>)};
-	const LinkedIn = () =>{return (
-		        <><h1>LinkedIn</h1><p>https://www.linkedin.com/in/yaroslav-moroz-4556a8225/</p></>)};
-    const GitHub = () =>{return (<><h1>GitHub</h1><p>ya.moroz.yaroslav@gmail.com</p></>)};
+	const Email = () =>{
+		return (<><h1 className={styles.label}>Email</h1>
+		          <p className={styles.discr}>ya.moroz.yaroslav@gmail.com</p></>)};
+
+	const LinkedIn = () =>{
+		   return ( <><h1 className={styles.label}>LinkedIn</h1>
+                      <p className={styles.discr}>
+                             linkedin.com/in/yaroslav-moroz-4556a8225/</p></>)};
+
+    const GitHub = () =>{
+    	return (<><h1 className={styles.label}>GitHub</h1>
+    	          <p className={styles.discr}>github.com/yaMorozYaroslav</p></>)};
+ 
+
  return(<div className={styles.can}>
                                <Number/>
                                <Email/>
