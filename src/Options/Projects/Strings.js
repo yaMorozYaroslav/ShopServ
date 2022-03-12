7543b674e5bc235776b409c7cf8c57a272817806
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-03-12 12:21:20 +0100

    31

--- src/Options/Projects/Strings.js
+++ src/Options/Projects/Strings.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export function Strings(){
-	const text1 = "My very first full stack project. Here is MERN Authorization. After registration you can add note directly to the database.";
+	const text1 = "My very first full stack project. Here is MERN Authorization. After registration you can add a note directly to the database.";
 	const text2 = "You can see the code of this application by clicking."
 	const text3 = "Reserved space.";
 	return [text1, text2, text3];
