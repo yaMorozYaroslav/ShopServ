96faf38842cc14ea3949e4ff543ff9e8258c9079
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2022-11-13 08:38:28 -0400

    842

--- src/Redux/openSlice.js
+++ src/Redux/openSlice.js
@@ -4,10 +4,12 @@ export const openSlice = createSlice({
 	name: 'open',
 	initialState: {alert:false, contact:false},
 	reducers: {
-		openAlert: state => {state.state = true},
-		closeAlert: state => {state.state = false}
+		openAlert: state => {state.alert = true},
+		closeAlert: state => {state.alert = false},
+		openContact: state => {state.contact = true},
+		closeContact: state => {state.contact = false}
 		}
 	})
 	console.log(openSlice.actions.openAlert())
-	export const {openAlert, closeAlert} = openSlice.actions
+	export const {openAlert, closeAlert, openContact, closeContact} = openSlice.actions
 	export default openSlice.reducer
